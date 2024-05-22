2948a2a1b75a37cd539d588ab8e29867e968ebe3
Lexa Huang <sixian.duidui@gmail.com>
2024-05-22 02:52:39 -0400

    implement login page frontend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { useEffect, useState } from "react";
 import LoginPage from "./LoginPage";
 import { getTokenFromUrl, getCurrentArtists } from "./api";
@@ -15,10 +15,8 @@ function App() {
   }, []);
 
   return (
-    <div className="App 
-    bg-[url('https://images.unsplash.com/photo-1584968203528-00005749189c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
-     bg-cover">
-      <header className="App-header">
+    <div className="min-h-screen flex flex-col items-center justify-center">
+
         {!token ? (
           <LoginPage />
         ) : (
@@ -31,7 +29,6 @@ function App() {
 
           </>
         )}
-      </header>
     </div>
   );
 }
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,20 +1,40 @@
 import React from "react";
 import { login } from "./api";
-
+import { TypeAnimation } from "react-type-animation";
 
 function LoginPage() {
-
   return (
     <>
-      <div className="window bg-white p-44 w-7/12 justify-center text-center">
-        <button 
-        // className="border-2 p-5 font-serif border-x-slate-500"
-        className = "bg-white hover:bg-emerald-300 text-gray-700 font-serif py-2 px-4 border border-gray-400 shadow"
-        
-        onClick={login}>Login with Spotify</button>
+      <div className="lg:px-28 xl:px-60 container">
+        <div className="window">
+          <div className="title-bar bg-indigo-400">
+            <button aria-label="Close" className="clos"></button>
+            <h1 className="title">Welcome to Yapify</h1>
+            <button aria-label="Resize" disabled className="hidden"></button>
+          </div>
+          <div className="separator"></div>
 
+          {/* <div className="details-bar bg-indigo-200">
+            <span>major</span>
+            <span>bag</span>
+            <span>alert</span>
+          </div> */}
+          <div className=" window-pane flex flex-col justify-center text-center items-center py-24">
+            <TypeAnimation
+              sequence={[
+                "Wanna get roasted for your impeccable music taste and yap with your favourite artists? Log in to spotify and come yap!",
+              ]}
+              speed={60}
+            />
+            <button
+              className="btn mt-5 bg-indigo-200 hover:bg-indigo-500 hover:text-white"
+              onClick={login}
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </div>
-      
     </>
   );
 }
